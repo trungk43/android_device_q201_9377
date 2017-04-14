c85b4b2d45b4d73f595803ca00f885d551649da7
Trung Nguyen Tran <Trung Nguyen Tran>
2017-04-14 07:36:55 +0700

    first commit

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+add_lunch_combo q201_9377-eng-32
+add_lunch_combo q201_9377-user-32
+add_lunch_combo q201_9377-userdebug-32
+add_lunch_combo q201_9377-eng-64
+add_lunch_combo q201_9377-user-64
+add_lunch_combo q201_9377-userdebug-64
